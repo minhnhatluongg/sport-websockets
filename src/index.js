b91921f25529e7a0af47f7d9a00a6597ef4de2d1
minhnhatluongg <cusocisme@gmail.com>
2026-03-12 14:45:38 +0700

    implement the websocket server

--- src/index.js
+++ src/index.js
@@ -1,21 +1,26 @@
 import express from 'express';
 import matchRouter from './routes/matches.js';
+import { attachWebSocketServer } from './ws/server.js';
+import http from 'http';
 
 const app = express();
+const server = http.createServer(app);
 const PORT = process.env.PORT || 8000;
+const HOST = process.env.HOST || '0.0.0.0';
 
 // Middleware
 app.use(express.json());
 
 // Routes
 app.use('/matches', matchRouter);
-
+const { broadcastMatchCreated } = attachWebSocketServer(server);
+app.locals.broadcastMatchCreated = broadcastMatchCreated;
 // Root route
 app.get('/', (req, res) => {
   res.json({ message: 'Sports API Server Running' });
 });
 
 // Start server
-app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+server.listen(PORT, HOST, () => {
+  console.log(`Server is running at http://${HOST}:${PORT}`);
 });
